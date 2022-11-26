5479267efea27ebf7355021a075ee22fb07c7cdc
Alex Koval <301019a@gmail.com>
2022-11-26 19:28:31 +0200

    task-03
    in progress...

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,48 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// створюємо елемент img шаблонними рядками
+// створюємо li шаблонними рядками
+
+
+
+
+
+const galleryEl = document.querySelector('.gallery');
+// const createGallery = images.map (image => {
+  
+
+// })
+
+
+const createGallery = images.forEach(el => {
+  galleryEl.insertAdjacentHTML(
+    'afterend',    
+   `<li><img class="picture" src="${el.url}" alt=${el.alt}></li>`,
+
+  );
+});
+
+
+
+// Завдання №3
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
