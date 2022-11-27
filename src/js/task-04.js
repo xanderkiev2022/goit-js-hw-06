cd813245eec0b1847916382f6c9abd439f634c78
Alex Koval <301019a@gmail.com>
2022-11-27 11:48:07 +0200

    task-04
    fin

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,39 +1,27 @@
-// const counter = {
-//     value: 0,
-//     increment() {
-//         console.log('increment -> this', this);
-//         this.value += 1;
-//     },
-//     decrement() {
-//         console.log('decrement -> this', this);
-//         this.value -= 1;
-//     },
-// };
+const counter = {
+    value: 0,                   // задаємо початкове значення лічильника
+    decrement(){                // через функцію змінюємо значення на -1
+        this.value -=1;
+    },
+    increment(){
+        this.value +=1;
+    },
 
-// const decrementBtn = document.querySelector('.js-decrement');
-// const incrementBtn = document.querySelector('.js-increment');
-// const valueEl = document.querySelector('.js-value');
+}
+const decrBtn = document.querySelector('[data-action="decrement"]'); // шукаємо якорі
+const incrBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
 
-// decrementBtn.addEventListener('click', function () {
-//     console.log('Кликнули на декремент');
+decrBtn.addEventListener('click', function(){                        // додаємо случхача
+    counter.decrement()                                              // запускаємо лічильник -
+    valueEl.textContent = counter.value;                             // перезаписуємо значення
+});
+incrBtn.addEventListener('click', function(){
+    counter.increment()
+    valueEl.textContent = counter.value;
+});
 
-//     counter.decrement();
-//     console.log(counter);
-//     valueEl.textContent = counter.value;
-// });
-
-// incrementBtn.addEventListener('click', function () {
-//     console.log('Кликнули на инкремент');
-
-//     counter.increment();
-//     console.log(counter);
-//     valueEl.textContent = counter.value;
-// });
-
-// console.log(window);
-
-
-
+// Завдання №4
 
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
