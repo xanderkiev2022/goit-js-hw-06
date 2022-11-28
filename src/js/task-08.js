19dfc3c8be64d896326e7198f2414e65332b1311
Alex Koval <301019a@gmail.com>
2022-11-28 12:25:12 +0200

    task-08
    fin

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,20 +1,29 @@
+const formEl = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+  event.preventDefault();                               // не перезагружаємо
+
+  //   console.log(event.currentTarget.elements);
+  const formAllEl = event.currentTarget.elements;       //перевіряємо існуючі елементи форми
+  const email = formAllEl.email.value;                  //дивимося значення емейлу
+  const password = formAllEl.password.value;
+
+  if (email === '' || password === '') {                //умова якщо строка пуста
+    alert('Всі поля повинні бути заповнені!');}
+
+  console.log({ email, password });                     //збираємо дані в об"єкт
+  event.currentTarget.reset();                          //очищуємо значення полів
+};
+
+formEl.addEventListener('submit', onFormSubmit);
+
 // Завдання 8
+
 // Напиши скрипт управління формою логіна.
-
-// <form class="login-form">
-//   <label>
-//     Email
-//     <input type="email" name="email" />
-//   </label>
-//   <label>
-//     Password
-//     <input type="password" name="password" />
-//   </label>
-//   <button type="submit">Login</button>
-// </form>
-
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
 // Під час відправлення форми сторінка не повинна перезавантажуватися.
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт,
+// де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
+// Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
